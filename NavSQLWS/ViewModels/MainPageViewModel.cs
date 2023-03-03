b2d92b7d932435da025de8ae5f75972070dbb11d
CristianS <Cristian.Sandu@exesoftware.ro>
2023-03-04 01:33:18 +0200

    Add readme and some clarifications

--- NavSQLWS/ViewModels/MainPageViewModel.cs
+++ NavSQLWS/ViewModels/MainPageViewModel.cs
@@ -10,7 +10,7 @@ namespace NavSQLWS.ViewModels;
 public partial class MainPageViewModel : BaseViewModel
 {
     [ObservableProperty]
-    ObservableCollection<MonkeyModel> monkeys;
+    List<MonkeyModel> monkeys;
 
     [ObservableProperty]
     MonkeyModel monkey;
@@ -24,49 +24,15 @@ public partial class MainPageViewModel : BaseViewModel
     {
         _dbConnection = dbConnection;
         Title = "Monkey Page";
-        Monkeys = new ObservableCollection<MonkeyModel>();
-        ToSaveOnDB = new MonkeyModel();
+        Monkeys = new List<MonkeyModel>();
+        ToSaveOnDB = new MonkeyModel(); // #added - instantiat mobelul 
         GetInitalDataCommand.Execute(null);
-
-        //Monkeys = new List<MonkeyModel>
-        //{
-        //    new MonkeyModel
-        //    {
-        //        Name = "Baboon",
-        //        Location = "Africa & Asia",
-        //        Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
-        //        Image = "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/baboon.jpg",
-        //        Population = 10000,
-        //        Latitude = -8.783195,
-        //        Longitude = 34.508523
-        //    },
-        //    new MonkeyModel
-        //    {
-        //        Name = "Capuchin Monkey",
-        //        Location = "Central & South America",
-        //        Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
-        //        Image = "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/capuchin.jpg",
-        //        Population = 23000,
-        //        Latitude = 12.769013,
-        //        Longitude = -85.602364
-        //    },
-        //     new MonkeyModel
-        //    {
-        //        Name = "Blue Monkey",
-        //        Location = "Central and East Africa",
-        //        Details = "The blue monkey or diademed monkey is a species of Old World monkey native to Central and East Africa, ranging from the upper Congo River basin east to the East African Rift and south to northern Angola and Zambia",
-        //        Image = "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/bluemonkey.jpg",
-        //        Population = 12000,
-        //        Latitude = 1.957709,
-        //        Longitude = 37.297204
-        //    },
-        //};
     }
 
     [RelayCommand]
     private async void GetInitalData()
     {
-        Monkeys = new ObservableCollection<MonkeyModel>(await _dbConnection.GetItemsAsync());
+        Monkeys = await _dbConnection.GetItemsAsync();
     }
 
     [RelayCommand]
@@ -98,6 +64,6 @@ public partial class MainPageViewModel : BaseViewModel
     private async void SaveOnDb()
     {
         await _dbConnection.SaveItemAsync(ToSaveOnDB);
-        Monkeys = new ObservableCollection<MonkeyModel>(await _dbConnection.GetItemsAsync());
+        Monkeys = await _dbConnection.GetItemsAsync();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# NavSQLWS
+# Workshop MVVM
+
+Link-uri utile:
+ - Inregistrare Workshop 4: https://youtu.be/53MoV21GcOI?wt.mc_id=studentamb_61984
+ - Cod Workshop: https://github.com/CristiSandu/NavigationWorkshop?wt.mc_id=studentamb_61984
+ - What is the MVVM pattern, What benefits does MVVM have? - https://www.youtube.com/watch?v=AXpTeiWtbC8
+ - https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/shell/tabs?view=net-maui-7.0?view=net-maui-7.0?wt.mc_id=studentamb_61984
+ - https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/shell/flyout?view=net-maui-7.0?view=net-maui-7.0?wt.mc_id=studentamb_61984
+ - https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/shell/navigation?view=net-maui-7.0?wt.mc_id=studentamb_61984
+ - Pub/Sub Messenger for .NET Applications with WeakReferenceMessenger - https://www.youtube.com/watch?v=vD17OetzGXc?wt.mc_id=studentamb_61984
+ - .NET MAUI: Push, Pop, & Pass Objects - https://www.youtube.com/watch?v=8z8qz-PePlc?wt.mc_id=studentamb_61984
+ - https://www.youtube.com/watch?v=7ZeZD-iJFQw
+ - Build mobile and desktop apps with .NET MAUI- https://learn.microsoft.com/en-us/training/paths/build-apps-with-dotnet-maui?wt.mc_id=studentamb_61984
+
+## Homework 
+
+![](/Img/hw.jpg)
+
+Observatii:
+- Cand apas pe save, sa se adauge instant in lista elementul din Entry 
+- Cand fac swipe to delete sa se stearga instant din lista elementul
+- Cand apas pe un element sa ma duc in pagina de edit in care pot face: update, delete
+- Cand apas pe plus sa ma duca in pagina de add in care pot face: add, delete sa fie disable (functional)
+- Datele sa fie stocate persistent folosind SQLite
+- Sa se faca update instant in lista cand adaugi sau modifici un element din alta pagina (Hint: https://www.youtube.com/watch?v=vD17OetzGXc?wt.mc_id=studentamb_61984)
+
+Bonus:
+- sa se taie textul cand elementul este done
+- sa se foloseasca popUp uri in locul paginilor de add si edit (Hint: [Popup Community Toolkits](https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/views/popup))   
+![](/Img/bonus.jpg)
+
+## Update SQLite 
+ Mai trebuie un Nuget instalat: **SQLitePCLRaw.bundle_green**
